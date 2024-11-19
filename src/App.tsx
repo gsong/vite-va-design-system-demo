521cc083037c374988165307071e2a824f29cf27
George Song <george@gsong.dev>
2024-11-18 20:19:42 -0800

    feat: remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useLayoutEffect } from "react";
 
 import { VaAlertExpandable } from "@department-of-veterans-affairs/component-library/dist/react-bindings";
 
+import "@department-of-veterans-affairs/formation/dist/formation.min.css";
+import "@department-of-veterans-affairs/component-library/dist/main.css";
+
 function App() {
   useLayoutEffect(() => {
     import("@department-of-veterans-affairs/web-components/loader").then(
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
-import "@department-of-veterans-affairs/formation/dist/formation.min.css";
-import "@department-of-veterans-affairs/component-library/dist/main.css";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
